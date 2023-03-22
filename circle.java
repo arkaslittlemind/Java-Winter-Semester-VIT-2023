a21e9834dff3f4e58eea333cf079e94590efb751
arkaslittlemind <arkadiptamojumder14@gmail.com>
2023-03-22 21:26:08 +0530

    master

---
+++ circle.java
@@ -0,0 +1,13 @@
+public class circle {
+    public double x, y;
+    public double r;
+
+
+    public double circumference() {
+        return 2 * 3.14 * r;
+    }
+
+    public double area() {
+        return 3.14 * r * r;
+    }
+}
