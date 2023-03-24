97209008d36504027f105628227daddac5de1bdb
arkaslittlemind <arkadiptamojumder14@gmail.com>
2023-03-24 19:23:02 +0530

    master

--- Example.java
+++ Example.java
@@ -9,3 +9,8 @@ public class Example {
         System.out.println("Out of try-catch block");
     }
 }
+
+// In this example, an array of size 5 is created and the 10th index is assigned a value of 50.
+// This will generate an exception as the array size is only 5. The exception is caught in the catch block and “Exception caught in try block” is printed. 
+//However, since there is no catch block for ArrayIndexOutOfBoundsException, 
+//it slips through to another scope and “Out of try-catch block” is printed. //
