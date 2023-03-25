e99514741d970e500a88dae4f285ae213802e292
arkaslittlemind <arkadiptamojumder14@gmail.com>
2023-03-25 19:37:23 +0530

    master

---
+++ uppercaseLowercase.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class uppercaseLowercase {
+     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str;
+        System.out.println("Enter a string: ");
+        str = sc.nextLine();
+        String str1 = str.toUpperCase();
+        String str2 = str.toLowerCase();
+        System.out.println("Uppercase: " + str1 + "\nLowercase: " + str2); 
+    }
+}
