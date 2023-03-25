de89c6b1e0db75c104b96aa7fd4a5817102a7c74
arkaslittlemind <arkadiptamojumder14@gmail.com>
2023-03-25 19:20:37 +0530

    master

--- sumOfcorrespondingdigits.java
+++ sumOfcorrespondingdigits.java
@@ -2,18 +2,19 @@ import java.util.Scanner;
 
 public class sumOfcorrespondingdigits {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter two numbers with the same number of digits: ");
-        int num1 = sc.nextInt();
-        int num2 = sc.nextInt();
-        int sum = 0;
-        while (num1 > 0) {
-            int digit1 = num1 % 10;
-            int digit2 = num2 % 10;
-            sum += digit1 * digit2;
-            num1 /= 10;
-            num2 /= 10;
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter two numbers with the same number of digits: ");
+            int num1 = sc.nextInt();
+            int num2 = sc.nextInt();
+            int sum = 0;
+            while (num1 > 0) {
+                int digit1 = num1 % 10;
+                int digit2 = num2 % 10;
+                sum += digit1 * digit2;
+                num1 /= 10;
+                num2 /= 10;
+            }
+            System.out.println("The sum of the product of corresponding digits is: " + sum);
         }
-        System.out.println("The sum of the product of corresponding digits is: " + sum);
     }
 }
