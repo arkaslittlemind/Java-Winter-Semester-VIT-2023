f1fcdbb850468442668aaf97a3277b288b5bea18
arkaslittlemind <arkadiptamojumder14@gmail.com>
2023-03-25 19:20:52 +0530

    master

--- SumOf2Numbers.java
+++ SumOf2Numbers.java
@@ -3,11 +3,12 @@ import java.util.Scanner;
 public class SumOf2Numbers {
     public static void main(String args[]) {
         int x, y, sum;
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the first number: ");
-        x = sc.nextInt();
-        System.out.println("Enter the second number: ");
-        y = sc.nextInt();
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter the first number: ");
+            x = sc.nextInt();
+            System.out.println("Enter the second number: ");
+            y = sc.nextInt();
+        }
         sum = sum(x, y);
         System.out.println("The sum of two numbers x and y is: " + sum);
     }
