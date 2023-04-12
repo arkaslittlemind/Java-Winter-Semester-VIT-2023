f0e959aada8869894d0d3f8ce589b65f4c79516d
arkaslittlemind <arkadiptamojumder14@gmail.com>
2023-04-12 22:36:45 +0530

    master

---
+++ ArrayIndex.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.util.Random.*;
+
+public class ArrayIndex {
+    public static void main(String[] args) {
+        int[] array = new int[100];
+        Random rand = new Random();
+        for (int i = 0; i < array.length; i++) {
+            array[i] = rand.nextInt(1000);
+        }
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the index of the array(0-99): ");
+        int index = sc.nextInt();
+
+        if (index < 0 || index >= array.length) {
+            System.out.println("Array Index Out of Bounds");
+        } else {
+            System.out.println("Element present at Index: " + index + " is" + " " + array[index]);
+        }
+    }
+}
