fec3673354d72a42ea086949ee324570d49911aa
arkaslittlemind <arkadiptamojumder14@gmail.com>
2023-04-13 23:09:33 +0530

    master

--- Employee.java
+++ Employee.java
@@ -8,6 +8,6 @@ class Programmer extends Employee {
     public static void main(String[] args) {
         Programmer p = new Programmer();
         System.out.println("Programmer Salary is:" + p.salary);
-        System.out.println("Bonus of Programmer is:" + p.bonus);
+        System.out.println("Bonus of the Programmer is:" + p.bonus);
     }
 }
