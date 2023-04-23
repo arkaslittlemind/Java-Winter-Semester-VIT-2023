6399ced539f72de921d8ea29ad9476318ab018f3
arkaslittlemind <arkadiptamojumder14@gmail.com>
2023-04-23 21:11:21 +0530

    master

--- PigLatin.java
+++ PigLatin.java
@@ -1,12 +1,12 @@
-public class PigLatin{
+public class PigLatin {
     static boolean isVowel(char ch) {
         return (ch == 'A' || ch == 'a' || ch == 'E' || ch == 'e' || ch == 'I' || ch == 'i' || ch == 'O' || ch == 'o'
                 || ch == 'U' || ch == 'u');
     }
 
-    // method that converts a word or string into Pig Latin
+    
     static String pigLatinWord(String string) {
-        // the index of the first vowel is stored
+        
         int stringlength = string.length();
         int index = -1;
         for (int i = 0; i < stringlength; i++) {
@@ -15,24 +15,23 @@ public class PigLatin{
                 break;
             }
         }
-        // Pig Latin is possible only if vowels is present
+        
         if (index == -1)
             return "-1";
-        // Take all characters after index (including index). Append all characters
-        // which are before index. Finally append "ay"
+        
         return string.substring(index) + string.substring(0, index) + "ay";
     }
 
-    // Driver code
+    
     public static void main(String args[]) {
-        String plstring1 = pigLatinWord("butter"); // begin with consonant
-        String plstring2 = pigLatinWord("apple"); // begin with vowel
-        String plstring3 = pigLatinWord("me"); // begin with consonant
-        String plstring4 = pigLatinWord("child"); // consonant cluster word
-        String plstring5 = pigLatinWord("egg"); // begin with vowel
-        String plstring6 = pigLatinWord("store"); // consonant cluster word
-        String plstring7 = pigLatinWord("eunoia"); // contains all vowels
-        String plstring8 = pigLatinWord("nymphly"); // contains no vowel
+        String plstring1 = pigLatinWord("butter"); 
+        String plstring2 = pigLatinWord("apple"); 
+        String plstring3 = pigLatinWord("me"); 
+        String plstring4 = pigLatinWord("child"); 
+        String plstring5 = pigLatinWord("egg"); 
+        String plstring6 = pigLatinWord("store"); 
+        String plstring7 = pigLatinWord("eunoia"); 
+        String plstring8 = pigLatinWord("nymphly"); 
         if (plstring1 == "-1")
             System.out.println("Pig Latin is not possible, the string must have at least a vowel.");
         else
