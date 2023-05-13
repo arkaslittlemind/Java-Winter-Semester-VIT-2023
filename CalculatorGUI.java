585cda165c2dea3831fe07cf433fab55397ce605
arkaslittlemind <arkadiptamojumder14@gmail.com>
2023-05-13 19:57:51 +0530

    master

--- CalculatorGUI.java
+++ CalculatorGUI.java
@@ -1,44 +1,36 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class CalculatorGUI extends JFrame {
+public class CalculatorGUI {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Calculator");
 
-    private JTextField displayField;
+        JPanel panel = new JPanel();
+        panel.setLayout(new BorderLayout());
 
-    public CalculatorGUI() {
-        // Set up the JFrame
-        super("Calculator");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(250, 300);
-        setLocationRelativeTo(null);
+        JTextField textField = new JTextField();
+        panel.add(textField, BorderLayout.NORTH);
 
-        // Set up the display field
-        displayField = new JTextField();
-        displayField.setEditable(false);
-        displayField.setFont(new Font("Arial", Font.PLAIN, 24));
-        displayField.setHorizontalAlignment(JTextField.RIGHT);
-        add(displayField, BorderLayout.NORTH);
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new GridLayout(4, 4));
 
-        // Set up the buttons panel
-        JPanel buttonsPanel = new JPanel(new GridLayout(4, 4, 5, 5));
-        String[] buttonLabels = {
+        String[] buttons = {
                 "7", "8", "9", "/",
                 "4", "5", "6", "*",
                 "1", "2", "3", "-",
                 "0", ".", "=", "+"
         };
-        for (String label : buttonLabels) {
-            JButton button = new JButton(label);
-            button.setFont(new Font("Arial", Font.PLAIN, 24));
-            buttonsPanel.add(button);
+
+        for (String button : buttons) {
+            JButton btn = new JButton(button);
+            buttonPanel.add(btn);
         }
-        add(buttonsPanel, BorderLayout.CENTER);
 
-        // Show the JFrame
-        setVisible(true);
-    }
+        panel.add(buttonPanel, BorderLayout.CENTER);
 
-    public static void main(String[] args) {
-        new CalculatorGUI();
+        frame.add(panel);
+        frame.setSize(300, 300);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
     }
 }
