595191e85f6a25faeaf09712ba767a11842d0bf1
arkaslittlemind <arkadiptamojumder14@gmail.com>
2023-05-15 21:58:00 +0530

    master

---
+++ JavaCollections/arrayList.java
@@ -0,0 +1,17 @@
+package JavaCollections;
+
+import java.util.ArrayList;
+
+public class arrayList {
+    public static void main(String[] args) {
+       ArrayList<Integer> numbers = new ArrayList<Integer>();
+
+       numbers.add(10);
+       numbers.add(20);
+       numbers.add(30);
+
+       System.out.println(numbers.get(0));
+
+    }
+}
+
