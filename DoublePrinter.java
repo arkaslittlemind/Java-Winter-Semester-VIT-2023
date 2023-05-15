a58338793db06e6e597780837df9b91e1ca5022a
arkaslittlemind <arkadiptamojumder14@gmail.com>
2023-05-15 12:04:45 +0530

    master

---
+++ DoublePrinter.java
@@ -0,0 +1,11 @@
+public class DoublePrinter {
+    Double thingToPrint;
+
+    public DoublePrinter(Double thingToPrint) {
+        this.thingToPrint = thingToPrint;
+    }
+
+    public void print() {
+        System.out.println(thingToPrint);
+    }
+}
--- GenericsExample.java
+++ GenericsExample.java
@@ -2,5 +2,8 @@ public class GenericsExample {
     public static void main(String[] args) {
         IntegerPrinter printer = new IntegerPrinter(23);
         printer.print();
+
+        DoublePrinter doublePrinter = new DoublePrinter(23.78);
+        doublePrinter.print();
     }
 }
