a45b18a561f928595df2d513fe263a3dbe8ef754
arkaslittlemind <arkadiptamojumder14@gmail.com>
2023-05-19 22:51:27 +0530

    master

---
+++ Multithreading/multi1.java
@@ -0,0 +1,15 @@
+public class multi1 {
+    public static void main(String[] args) {
+        
+        
+        MyThread thread1 = new MyThread();
+
+        
+        MyRunnable runnable1 = new MyRunnable();
+        Thread thread2 = new Thread(runnable1);
+
+        thread1.start();
+        thread2.start();
+
+    }
+}
