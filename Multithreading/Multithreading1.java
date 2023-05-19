8d226b9425b621ff7864817b4aa3864844156e81
arkaslittlemind <arkadiptamojumder14@gmail.com>
2023-05-19 23:19:03 +0530

    master

--- Multithreading/Multithreading1.java
+++ Multithreading/Multithreading1.java
@@ -8,9 +8,13 @@ public class Multithreading1 {
         MyRunnable runnable1 = new MyRunnable();
         Thread thread2 = new Thread(runnable1);
 
+        thread1.setDaemon(true);
+        thread2.setDaemon(true);
+        
         thread1.start();
-        thread1.join();
+        //thread1.join(3000); // calling thread waits until the specified thread dies or for x amount of milliseconds 
         thread2.start();
 
+        System.out.println(1/10);
     }
 }
